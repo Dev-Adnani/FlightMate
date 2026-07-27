72804ea5a3e0f5df09a193b0c1af8fface8e9bf7
Dev Adnani <dev.adnani26@gmail.com>
2026-07-27 16:41:05 +0530

    Initial Commit

---
+++ FlightMate/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  FlightMate
+//
+//  Created by Dev Adnani on 27/07/26.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ FlightMateTests/FlightMateTests.swift
@@ -0,0 +1,19 @@
+//
+//  FlightMateTests.swift
+//  FlightMateTests
+//
+//  Created by Dev Adnani on 27/07/26.
+//
+
+import Testing
+@testable import FlightMate
+
+struct FlightMateTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
